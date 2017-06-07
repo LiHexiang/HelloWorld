1019c71e2c0112be0204fe86c3e9ca114c7b817d
LiHexiang <lihexiang2012@163.com>
2017-06-07 10:50:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lihexiang/helloworld/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.lihexiang.helloworld
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+//    val btToast: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    
+}
